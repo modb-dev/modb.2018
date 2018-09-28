96774b508402821f1b9f46b3b75eb60c94b4ead9
Andrew Chilton <andychilton@gmail.com>
2018-09-29 11:21:58 +1200

    Remove temporary databases after tests

--- store/store_test.go
+++ store/store_test.go
@@ -20,6 +20,8 @@ func TestStore(t *testing.T) {
 
 	// create ServerService
 	bboltPath := tmpName()
+	defer os.Remove(bboltPath)
+
 	bboltStore, err := bbolt.Open(bboltPath)
 	if err != nil {
 		t.Fatal(err)
@@ -28,6 +30,8 @@ func TestStore(t *testing.T) {
 
 	// create ServerService
 	badgerPath := tmpName()
+	defer os.RemoveAll(badgerPath)
+
 	badgerStore, err := badger.Open(badgerPath)
 	if err != nil {
 		t.Fatal(err)
@@ -47,11 +51,19 @@ func TestStore(t *testing.T) {
 func ServerServiceTests(t *testing.T, store modb.ServerService) {
 
 	t.Run("Set", func(t *testing.T) {
-
+		err := store.Set("account/chilts", "{}")
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
 	})
 
 	t.Run("Inc", func(t *testing.T) {
-
+		err := store.Inc("account/chilts", "logins")
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
 	})
 
 }
